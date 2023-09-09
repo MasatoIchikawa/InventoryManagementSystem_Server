0e3475816917edd1a98c03b73bb0ab0a2d1f55c1
masato.ichikawa <masato.ichikawa.engineer@gmail.com>
2023-09-09 18:30:11 +0900

    wip

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const port = 3001;
 
 app.use(express.json());
 
+const poolOption = {
+    connectionLimit : 1,
+    host: 'localhost',
+    user: 'root',
+    password: 'xeno1508',
+    database: 'stock',
+    timezone: 'jst'
+};
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -18,9 +27,13 @@ const connection = mysql.createConnection({
 
 app.get('/mstinventory', (req, res) => {
     const sql = `SELECT * FROM m_inventory
-                 WHERE delete_at IS NULL`;
+    WHERE delete_at IS NULL
+    AND update_at IS NULL`;
     connection.query(sql, function (err, result, fields) {
-        if (err) throw err;
+        if (err) {
+            connection.rollback(() => err);
+            throw err;
+        }
         res.status(200).json(result);
     });
 });
@@ -39,8 +52,41 @@ app.get('/mstinventory_edit', (req, res) => {
         });
 });
 
-app.post('/mstinventory_insert', (req, res) => {
-    const sql = `INSERT m_inventory(
+app.post('/mstinventory_insert', async (req, res, next) => {
+    const pool = mysql.createPool(poolOption);
+    const connection = await new Promise((resolve, reject) => {
+        pool.getConnection((error, connection) => {
+          if (error) reject(error)
+          resolve(connection)
+        })
+      })
+
+      try{
+          await new Promise((resolve, reject) => {
+            connection.beginTransaction((error, results) => {
+              if (error) reject(error)
+              resolve(results)
+            })
+          })
+          console.log('=== done beginTransaction ===')
+
+          await new Promise((resolve, reject) => {
+            const update = `UPDATE m_inventory
+            SET update_at = NOW()
+            WHERE inventory_id = ?`;
+
+            const updateparam = [
+                req.body.inventory_id,
+            ];
+            connection.query(update, updateparam, (error, results) => {
+              if (error) reject(error)
+              resolve(results)
+            })
+          })
+          console.log('=== done update ===');
+
+          await new Promise((resolve, reject) => {
+            const insert = ` INSERT m_inventory(
                 inventory_name,
                 inventory_kana,
                 picture, 
@@ -61,32 +107,55 @@ app.post('/mstinventory_insert', (req, res) => {
                 delete_at,
                 insert_user_id)
                 VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NULL, NULL, ?)`;
-    connection.query(sql, [
-        req.body.inventory_name,
-        req.body.inventory_kana,
-        req.body.picture,
-        req.body.category_id,
-        req.body.jancode,
-        req.body.skucode,
-        req.body.unit_id,
-        req.body.price,
-        req.body.price_cost,
-        req.body.inventory_max,
-        req.body.inventory_min,
-        req.body.location,
-        req.body.url,
-        req.body.note,
-        req.body.display_flag,
-        req.body.insert_user_id
-    ],
-        (error, result) => {
-            if(error) {
-                console.log(error)
-            }
-            else{
-                res.status(200).json(result);
-            }
-          });
+
+            const insertparam = [
+                req.body.inventory_name,
+                req.body.inventory_kana,
+                req.body.picture,
+                req.body.category_id,
+                req.body.jancode,
+                req.body.skucode,
+                req.body.unit_id,
+                req.body.price,
+                req.body.price_cost,
+                req.body.inventory_max,
+                req.body.inventory_min,
+                req.body.location,
+                req.body.url,
+                req.body.note,
+                req.body.display_flag,
+                req.body.insert_user_id
+            ];
+            connection.query(insert, insertparam, (error, results) => {
+              if (error) reject(error)
+              resolve(results)
+            })
+          })
+          console.log('=== done insert ===');
+
+          await new Promise((resolve, reject) => {
+            connection.commit((error, results) => {
+              if (error) reject(error)
+              resolve(results)
+            })
+          })
+          console.log('=== done commit ===');
+      }
+      catch{
+        await new Promise((resolve, reject) => {
+            connection.rollback((error, results) => {
+              if (error) reject(error)
+              resolve(results)
+            })
+          })
+          console.log('=== done rollback ===')
+      }
+      finally{
+        connection.release();
+        console.log('=== done release ===');
+        pool.end();
+        console.log('=== done poolend ===');
+      }
 });
 
 app.post('/mstinventory_delete', (req, res) => {
